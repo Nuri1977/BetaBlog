c57acfc3a2ed41bf5ab7a56eebee21d0d6041f7f
nuri1977 <nurilacka@gmail.com>
2019-12-24 00:39:01 +0100

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  validates :username, presence: true, 
+            uniqueness: {case_sensitive: false},
+            length: {minimum: 3, maximum: 25 }
+
+  VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i       
+  validates :email, presence: true, 
+            uniqueness: {case_sensitive: false},
+            length: {minimum: 3, maximum: 105 },
+            format: { with: VALID_EMAIL_REGEX }
+
 end
