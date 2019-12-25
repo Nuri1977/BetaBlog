ec49256d819cab969d9e319f5f53814ac2201571
nuri1977 <nurilacka@gmail.com>
2019-12-25 21:33:10 +0100

    add gravatar and user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,24 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user = User.update(user_params)
+      flash[:succes] = " Your account was updated succesfully"
+      redirect_to articles_path
+    else
+      render 'show'
+    end
+  end
   
   private
   def user_params
