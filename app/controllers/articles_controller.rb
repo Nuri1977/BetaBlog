ca642cefcead799d9865e9a2c18380a1cacc582d
nuri1977 <nurilacka@gmail.com>
2019-12-28 23:40:19 +0100

    update current users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article=Article.new(novo_params)
-    @article.user = User.find(session[:user_id]) if session[:user_id]
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
