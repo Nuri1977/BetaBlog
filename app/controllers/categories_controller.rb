0c9d9e5a9e26460d51c4eab792ac9fa468a203bc
nuri1977 <nurilacka@gmail.com>
2019-12-30 02:04:12 +0100

    edit categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -26,6 +26,17 @@ class CategoriesController < ApplicationController
   end
 
   def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category name was successfully updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
   end
 
   private
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ApplicationRecord
   has_many :article_categories
   has_many :categories, through: :article_categories
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
-  validates :description, presence: true, length: {minimum: 10, maximum: 600}
+  validates :description, presence: true, length: {minimum: 10, maximum: 2000}
   validates :user_id, presence: true
 end
